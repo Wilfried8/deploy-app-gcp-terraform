35d0fc1b225d1bba2f3940d804ca643cfe8b3763
wilfried tayo <wktayo@gmail.com>
2023-10-05 12:37:15 +0200

    gestion des proxy

--- infra/main.tf
+++ infra/main.tf
@@ -20,8 +20,67 @@ resource "google_storage_bucket_object" "static_site_src" {
   bucket = google_storage_bucket.website.name
 }
 
-# Reserve an external IP
+# Reserve an  static external IP
 resource "google_compute_global_address" "website_ip" {
   provider = google
   name     = "website-lb-ip"
+}
+
+# Get the managed DNS zone
+data "google_dns_managed_zone" "dns_zone" {
+  provider = google
+  name     = "terraform-gcp"
+}
+
+# Add the IP to the DNS
+resource "google_dns_record_set" "website" {
+  provider     = google
+  name         = "website.${data.google_dns_managed_zone.dns_zone.dns_name}"
+  type         = "A"
+  ttl          = 300
+  managed_zone = data.google_dns_managed_zone.dns_zone.name
+  rrdatas      = [google_compute_global_address.website_ip.address]
+}
+
+# Add the bucket as a CDN backend
+resource "google_compute_backend_bucket" "website-backend" {
+  provider    = google
+  name        = "website-backend"
+  description = "Contains files needed by the website"
+  bucket_name = google_storage_bucket.website.name
+  enable_cdn  = true
+}
+
+# GCP URL MAP
+resource "google_compute_url_map" "website" {
+  provider        = google
+  name            = "website-url-map"
+  default_service = google_compute_backend_bucket.website-backend.self_link
+    host_rule {
+    hosts        = ["*"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name            = "allpaths"
+    default_service = google_compute_backend_bucket.website-backend.self_link
+  }
+}
+
+# GCP target proxy
+resource "google_compute_target_https_proxy" "website" {
+  provider         = google
+  name             = "website-target-proxy"
+  url_map          = google_compute_url_map.website.self_link
+}
+
+# GCP forwarding rule
+resource "google_compute_global_forwarding_rule" "default" {
+  provider              = google
+  name                  = "website-forwarding-rule"
+  load_balancing_scheme = "EXTERNAL"
+  ip_address            = google_compute_global_address.website_ip.address
+  ip_protocol           = "TCP"
+  port_range            = "80"
+  target                = google_compute_target_https_proxy.website.self_link
 }
